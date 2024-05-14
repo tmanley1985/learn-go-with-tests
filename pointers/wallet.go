12ce20483b02204bf249c43c0588c0131c433e19
Thomas Manley <tmanley1985@gmail.com>
2024-05-14 16:21:54 -0400

    Completed

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,17 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-type Wallet struct {
-	balance int
+type Bitcoin int
+
+// We're implementing this interface:
+// https://pkg.go.dev/fmt#Stringer
+// Remember, that we don't have to explicitly state that
+// Bitcoin implements the interface, we just have to actually
+// implement the interface structurally.
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in test is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	// We could have written this like so: return (*w).balance
 	// and that would be an explicit dereference. But in Go they
 	// thought this was cumbersome. So they made it to where it's an implicit
@@ -19,4 +33,16 @@ func (w *Wallet) Balance() int {
 	// Also, I don't actually have to use the pointer receiver here as the copy
 	// of wallet would have been fine. However, for consistency I'll keep it.
 	return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,21 +1,56 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
-
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-	wallet.Deposit(10)
+	t.Run("withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
 
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, Bitcoin(20))
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
-	want := 10
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
