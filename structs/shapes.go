8ff66725e05aae4094b8bfe28f64e6777af9ce24
Thomas Manley <tmanley1985@gmail.com>
2024-05-14 14:25:20 -0400

    Completed

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,38 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Shape interface {
+	Area() float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * .5 * t.Height
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,10 +13,28 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
+
+	for _, test := range areaTests {
+		// Giving t.Run a test name is good for readability.
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Area()
+			if got != test.hasArea {
+				// The v here will put out the value of our structs.
+				// g will put a very precise number which is good for math operations.
+				t.Errorf("%#v got %g want %g", test.shape, got, test.hasArea)
+			}
+		})
+
+	}
+
 }
