55383c3debf82f30ce492573f90a6ef98345ff2e
Thomas Manley <tmanley1985@gmail.com>
2024-05-22 15:40:34 -0400

    Added a test for the Racer function, using a mock server

---
+++ select/racer_test.go
@@ -0,0 +1,35 @@
+package concurrency
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	// Here we're creating a mock server so that we can control the speed and flakiness of a real request.
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Obviously this is where we can introduce latency with a sleep.
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
