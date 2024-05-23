2c21b31a0955d6019e08e33634fef5332cd25824
Thomas Manley <tmanley1985@gmail.com>
2024-05-23 16:07:16 -0400

    Using switch

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,28 +7,24 @@ func walk(x interface{}, fn func(input string)) {
 	// actual value to run the function over!
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
+	// Why do we do this .Interface() thing?
+	// Well, think about it, we don't know what the type
+	// is here, so we could either figure it out or pass the
+	// value in as an empty interface. The walk function already
+	// handles the figuring out of the type anyway, so we don't have to.
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
-			// Why do we do this .Interface() thing?
-			// Well, think about it, we don't know what the type
-			// is here, so we could either figure it out or pass the
-			// value in as an empty interface. The walk function already
-			// handles the figuring out of the type anyway, so we don't have to.
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
+	case reflect.String:
+		fn(val.String())
 	}
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
-	}
 }
 
 func getValue(x interface{}) reflect.Value {
