63e971eac7157ce22176ba6d24d8ed33b106059c
Thomas Manley <tmanley1985@gmail.com>
2024-05-23 16:23:57 -0400

    Handling maps, channels and functions

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,22 +7,44 @@ func walk(x interface{}, fn func(input string)) {
 	// actual value to run the function over!
 	val := getValue(x)
 
-	// Why do we do this .Interface() thing?
-	// Well, think about it, we don't know what the type
-	// is here, so we could either figure it out or pass the
-	// value in as an empty interface. The walk function already
-	// handles the figuring out of the type anyway, so we don't have to.
+	walkValue := func(value reflect.Value) {
+		// Why do we do this .Interface() thing?
+		// Well, think about it, we don't know what the type
+		// is here, so we could either figure it out or pass the
+		// value in as an empty interface. The walk function already
+		// handles the figuring out of the type anyway, so we don't have to.
+		walk(value.Interface(), fn)
+	}
+
 	switch val.Kind() {
-	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
-		}
-	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
 	case reflect.String:
 		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		// We just iterate forever until the channel is closed.
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
 	}
 
 }
