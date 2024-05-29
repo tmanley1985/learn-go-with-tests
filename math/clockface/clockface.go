9ba37473b1feb3522a86720034218ff114f49322
Thomas Manley <tmanley1985@gmail.com>
2024-05-29 16:56:40 -0400

    Scaled, flipped and translated

--- math/clockface/clockface.go
+++ math/clockface/clockface.go
@@ -11,13 +11,17 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}   // scale: the length of our second hand is 90. This is just a convention.
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}   // scale: the length of our second hand is 90. This is just a convention.
 	p = Point{p.X, -p.Y}            // flip: to account for the SVG having an origin in the top left hand corner.
-	p = Point{p.X + 150, p.Y + 150} // translate: the origin needs to be the center of the clock at 150, 150!
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY} // translate: the origin needs to be the center of the clock at 150, 150!
 	return p
 }
 func secondsInRadians(t time.Time) float64 {
