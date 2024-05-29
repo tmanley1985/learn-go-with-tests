e4438e523937ff0f739cc64dcb71b0a953085cd3
Thomas Manley <tmanley1985@gmail.com>
2024-05-29 16:55:00 -0400

    Scaled, flipped and translated

--- math/clockface/clockface.go
+++ math/clockface/clockface.go
@@ -14,9 +14,12 @@ type Point struct {
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+	p := secondHandPoint(t)
+	p = Point{p.X * 90, p.Y * 90}   // scale: the length of our second hand is 90. This is just a convention.
+	p = Point{p.X, -p.Y}            // flip: to account for the SVG having an origin in the top left hand corner.
+	p = Point{p.X + 150, p.Y + 150} // translate: the origin needs to be the center of the clock at 150, 150!
+	return p
 }
-
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
--- math/clockface/clockface_acceptance_test.go
+++ math/clockface/clockface_acceptance_test.go
@@ -1,5 +1,12 @@
 package clockface_test
 
+import (
+	"testing"
+	"time"
+
+	"github.com/tmanley1985/learn-go-with-tests/math/clockface"
+)
+
 // func TestSecondHandAtMidnight(t *testing.T) {
 // 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
@@ -11,13 +18,13 @@ package clockface_test
 // 	}
 // }
 
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
 
-// 	want := clockface.Point{X: 150, Y: 150 + 90}
-// 	got := clockface.SecondHand(tm)
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
 
-// 	if got != want {
-// 		t.Errorf("Got %v, wanted %v", got, want)
-// 	}
-// }
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
