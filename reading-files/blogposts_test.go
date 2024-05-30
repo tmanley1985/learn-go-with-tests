3f3e1c8bd2e2f85b4bc384074ce08089c009278f
Thomas Manley <tmanley1985@gmail.com>
2024-05-30 15:59:42 -0400

    Using an interface to read blog posts

---
+++ reading-files/blogposts_test.go
@@ -0,0 +1,26 @@
+package blogposts_test
+
+import (
+	"testing"
+	"testing/fstest"
+
+	blogposts "github.com/tmanley1985/learn-go-with-tests/reading-files"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+}
