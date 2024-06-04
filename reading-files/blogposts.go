5629ba27cb292c5f316852948dc856acf447485f
Thomas Manley <tmanley1985@gmail.com>
2024-06-04 17:17:57 -0400

    Using an interface for a reader

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -11,40 +11,35 @@ type Post struct {
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
-
 	if err != nil {
 		return nil, err
 	}
-
 	var posts []Post
 	for _, f := range dir {
-		post, err := getPost(fileSystem, f)
-
+		post, err := getPost(fileSystem, f.Name())
 		if err != nil {
-			return nil, err
+			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore?
 		}
-
 		posts = append(posts, post)
 	}
 	return posts, nil
 }
 
-func getPost(filesystem fs.FS, f fs.DirEntry) (Post, error) {
-	postfile, err := filesystem.Open(f.Name())
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
 
+func newPost(postFile io.Reader) (Post, error) {
+	postData, err := io.ReadAll(postFile)
 	if err != nil {
 		return Post{}, err
 	}
 
-	defer postfile.Close()
-
-	postData, err := io.ReadAll(postfile)
-
-	if err != nil {
-		return Post{}, err
-	}
-
-	post := Post{Title: string(postData[7:])}
-
+	post := Post{Title: string(postData)[7:]}
 	return post, nil
 }
