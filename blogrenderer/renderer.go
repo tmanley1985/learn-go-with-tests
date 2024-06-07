b55145f078aaf4d7ea4ac252a6fdcf7fdc0fa6f8
Thomas Manley <tmanley1985@gmail.com>
2024-06-07 18:25:07 -0400

    Using templates now

--- blogrenderer/renderer.go
+++ blogrenderer/renderer.go
@@ -1,36 +1,24 @@
 package blogrenderer
 
 import (
-	"fmt"
 	"io"
+	"text/template"
 
 	blogposts "github.com/tmanley1985/learn-go-with-tests/reading-files"
 )
 
+const (
+	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+)
+
 func Render(w io.Writer, p blogposts.Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Description)
+	templ, err := template.New("blog").Parse(postTemplate)
 
 	if err != nil {
 		return err
 	}
 
-	_, err = fmt.Fprintf(w, "Tags: <ul>")
-
-	if err != nil {
-		return err
-	}
-
-	for _, tag := range p.Tags {
-		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
-
-		if err != nil {
-			return err
-		}
-	}
-
-	_, err = fmt.Fprintf(w, "</ul>")
-
-	if err != nil {
+	if err := templ.Execute(w, p); err != nil {
 		return err
 	}
 
